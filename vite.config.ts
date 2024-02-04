7166bb442a9a9b9d6b1b84674b78e3452662e18d
Archisha Bhattacharya <aba406@uregina.ca>
2024-02-03 23:06:12 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,10 @@
 import { defineConfig } from 'vite'
-import { resolve } from 'path'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   port: parseInt(process.env.PORT, 10) || 3001,
-  // },
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve('./index.html'),
-      },
-    },
+  server: {
+    port: parseInt(process.env.PORT, 10) || 3001,
   },
   plugins: [react()],
 })
