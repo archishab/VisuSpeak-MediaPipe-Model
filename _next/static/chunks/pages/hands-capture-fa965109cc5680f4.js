0d18735aca0a60add85f9e0146c9b981608f075f
Archisha Bhattacharya <aba406@uregina.ca>
2024-02-23 00:26:51 -0600

    update bg width

--- _next/static/chunks/pages/hands-capture-fa965109cc5680f4.js
+++ _next/static/chunks/pages/hands-capture-fa965109cc5680f4.js
@@ -173,7 +173,7 @@
                         t.fillRect(
                           h * (e = Math).min.apply(e, (0, i.Z)(b)) - 10,
                           d * (r = Math).min.apply(r, (0, i.Z)(N)) - textHeight - 20,
-                          textSize.width + 100,
+                          (_ = Math).max.apply(_, (0, i.Z)(b)) - (w = Math).min.apply(w, (0, i.Z)(b)),
                           textHeight + 10
                       ),
                           (t.fillStyle = "white"),
